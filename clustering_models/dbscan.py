f09a30d1ca6cbfe56babfd840b6bf1dc2a49549c
Abhinav Rungta <abhinavrungta89@gmail.com>
2015-11-26 08:44:40 -0500

    Trying to fix dbscan

--- clustering_models/dbscan.py
+++ clustering_models/dbscan.py
@@ -12,35 +12,35 @@ from pyspark.sql.functions import UserDefinedFunction
 from pyspark.sql.types import StructType, ArrayType, FloatType, StructField, Row
 from sklearn.cluster import DBSCAN
 
-
-global_db = None
-def getDistance(x1, y1, x2, y2 ):
+def getDistance(x1, y1, x2, y2):
     return vincenty((x1, y1) , (x2, y2)).miles
     
-def getLocationsOfUser(userId):
-    locations = global_db[global_db["user_id"] == userId]
+def getLocationsOfUser(business_db, userId):
+    locations = business_db[business_db["user_id"] == userId]
     list = []
     for index, row in locations.iterrows():
         list.append(Row(latitude=row['latitude'], longitude=row['longitude']))
-    print('HI')
-    print(list)
     return list
     # self.sqlContext.sql("SELECT latitude, longitude FROM userBusiness where user_id = \"{0}\"".format(userId)).collect()        
     
 def getCentersOfUser(self, locations):
     size = locations.length
     distance_matrix = numpy.zeros((size, size))
-    for x in range(0, size-1):
-        for y in range(x+1, size-1):
+    for x in range(0, size - 1):
+        for y in range(x + 1, size - 1):
             pointA = locations[x]
             pointB = locations[y]
             distance_matrix[x][y] = getDistance(pointA.latitude, pointA.longitude, pointB.latitude, pointB.longitude)
-            
+
+    # list.append(Row(latitude=row['latitude'], longitude=row['longitude']))            
     db = DBSCAN(eps=3, min_samples=10, metric='precomputed').fit(distance_matrix)
     return db.components_
 
 class MainApp(object):
     def __init__(self):
+        pass
+    
+    def init(self):
         os.environ["SPARK_HOME"] = "/Users/abhinavrungta/Desktop/setups/spark-1.5.2"
         # os.environ['AWS_ACCESS_KEY_ID'] = <YOURKEY>
         # os.environ['AWS_SECRET_ACCESS_KEY'] = <YOURKEY>
@@ -50,11 +50,8 @@ class MainApp(object):
         conf.set("spark.executor.memory", "3g")
         self.sc = SparkContext(conf=conf)
         self.sqlContext = SQLContext(self.sc)
-        self.df_review = self.sqlContext.read.json("../yelp_dataset_challenge_academic_dataset/yelp_academic_dataset_review.json").cache()
-        self.df_business = self.sqlContext.read.json("../yelp_dataset_challenge_academic_dataset/yelp_academic_dataset_business.json").cache()
-        self.df_review.registerTempTable("reviews")
-        self.df_business.registerTempTable("business")
         
+    def init2(self):
         schema = StructType([
             StructField("latitude", FloatType()),
             StructField("longitude", FloatType())
@@ -62,7 +59,12 @@ class MainApp(object):
         self.get_locations = UserDefinedFunction(getLocationsOfUser, ArrayType(schema))
         self.get_centers = UserDefinedFunction(getCentersOfUser, ArrayType(schema))
         
-    
+    def loadData(self):
+        self.df_review = self.sqlContext.read.json("../yelp_dataset_challenge_academic_dataset/yelp_academic_dataset_review.json").cache()
+        self.df_business = self.sqlContext.read.json("../yelp_dataset_challenge_academic_dataset/yelp_academic_dataset_business.json").cache()
+        self.df_review.registerTempTable("reviews")
+        self.df_business.registerTempTable("business")
+        
     def createCheckInDataPerUser(self):
         review_user = self.sqlContext.sql("SELECT business_id, user_id FROM reviews")
         business_loc = self.sqlContext.sql("SELECT business_id, latitude, longitude FROM business")
@@ -75,13 +77,13 @@ class MainApp(object):
         self.df_unique_users = self.sqlContext.sql("SELECT DISTINCT user_id FROM userBusiness")
         self.df_unique_users.registerTempTable("users")
         
-        pd = self.df_join_reviewAndBusiness.toPandas()        
+        pd = self.df_join_reviewAndBusiness.toPandas()
         global_db = self.sc.broadcast(pd)
-        
-        self.df_unique_users.withColumn("user_locations", self.get_locations(self.df_unique_users["user_id"]))
+    
+        self.df_unique_users.withColumn("user_locations", self.get_locations(global_db.value, self.df_unique_users["user_id"]))
         self.df_unique_users.registerTempTable("users")
         
-        self.df_unique_users.repartition(1).save("user.json","json")
+        self.df_unique_users.repartition(1).save("user.json", "json")
 
         self.df_unique_users.withColumn("user_centers", self.get_centers(self.df_unique_users["user_locations"]))
         self.df_unique_users.registerTempTable("users")
@@ -91,6 +93,9 @@ class MainApp(object):
 
 def main():
         app = MainApp()
+        app.init()
+        app.init2()
+        app.loadData()
         app.createCheckInDataPerUser()
         # app.createFeatures()
 
